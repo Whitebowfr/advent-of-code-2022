403dcf0397fc6f3b1dc45b701c74323315669676
Whitebow <whitebowfr@gmail.com>
2022-12-01 13:32:52 +0100

    Changed varaible names, added one liners

--- day1.ts
+++ day1.ts
@@ -1,21 +1,35 @@
 import { day1 } from "./@lib";
-import { sum, parseStringAsInts } from "./@lib";
+import { sum, parseStringAsInts, sort } from "./@lib";
 
 function addCalories(data: string): number[] {
-    let dataSplitted: string[] = data.split("\n\n")
-    let output: number[] = dataSplitted.map(x => sum(parseStringAsInts(x)))
-    return output.sort((a, b) => a > b ? -1 : 1)
+    let snacks: string[] = data.split("\n\n")
+    let caloriesPerDeer: number[] = snacks.map(x => sum(parseStringAsInts(x)))
+    return sort(caloriesPerDeer, false)
 }
 
 function partOne(data: string): number|void {
-    let output = addCalories(data)
-    console.log(output[0])
-    return output[0]
+    let caloriesPerDeer = addCalories(data)
+    console.log(caloriesPerDeer[0])
+    return caloriesPerDeer[0]
 }
 
 function partTwo(data: string): number|void {
-    let out = addCalories(data)
-    console.log(sum(out, 3))
-    return sum(out, 3)
+    let caloriesPerDeer = addCalories(data)
+    console.log(sum(caloriesPerDeer, 3))
+    return sum(caloriesPerDeer, 3)
 }
-partTwo(day1)
+
+// One-liners because why not
+const oneLinerPartOne = (data: string) => data.split("\n\n")
+    .map(x => x.split("\n")
+        .map(y => parseInt(y))
+        .reduce((a, b) => a + b))
+    .sort((a, b) => b - a)[0]
+
+const oneLinerPartTwo = (data: string) => data.split("\n\n")
+    .map(x => x.split("\n")
+        .map(y => parseInt(y))
+        .reduce((a, b) => a + b))
+    .sort((a, b) => b - a)
+    .splice(0, 3)
+    .reduce((a, b) => a + b)

585f01da65d8adbb2a35fe100b2b179092e52fe4
Whitebow <whitebowfr@gmail.com>
2022-12-01 13:32:40 +0100

    Added sort function

--- @lib/arrays/index.ts
+++ @lib/arrays/index.ts
@@ -5,3 +5,15 @@ export function parseStringAsInts(str: string, separator="\n"): number[] {
 export function parseAsInts(a: any[]): number[] {
     return a.map(x => parseInt(x))
 }
+
+export function sort(a: any[], ascending=true, property?: string): any[] {
+    if (!property) {
+        return a.sort((a, b) => ((a > b && ascending) || (a < b && !ascending)) ? 1 : -1)
+    } else {
+        if ((a[0] as Object).hasOwnProperty(property)) {
+            return sort(a.map(x => x[property]), ascending)
+        } else {
+            throw new Error("Object does not have property " + property)
+        }
+    }
+}

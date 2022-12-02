480f7e27859097e0908616a589eee1677e427a1d
Whitebow <whitebowfr@gmail.com>
2022-12-02 11:55:31 +0100

    Streamlined the win calculation and wrote a one-liner

--- day2.ts
+++ day2.ts
@@ -1,4 +1,4 @@
-import { day2 } from "./@lib";
+import { day2, getIndexCircular, sum } from "./@lib";
 
 enum Shape {
     ROCK = 1,
@@ -40,62 +40,52 @@ function parseDataPtTwo(data: string): [Shape, Outcomes][] {
 
 
 function decodeWin(opponent: Shape, player: Shape): Outcomes {
-    switch(opponent) {
-        case Shape.ROCK:
-            switch(player) {
-                case Shape.ROCK: return Outcomes.DRAW
-                case Shape.PAPER: return Outcomes.WIN
-                default: return Outcomes.LOSS
-            }
-        case Shape.PAPER:
-            switch(player) {
-                case Shape.ROCK: return Outcomes.LOSS
-                case Shape.PAPER: return Outcomes.DRAW
-                default: return Outcomes.WIN
-            }
-        case Shape.SCISSORS:
-            switch(player) {
-                case Shape.ROCK: return Outcomes.WIN
-                case Shape.PAPER: return Outcomes.LOSS
-                default: return Outcomes.DRAW
-            }
-    }
+    if (opponent === player) return Outcomes.DRAW
+    if (getWinningType(opponent, Outcomes.LOSS) === player) return Outcomes.LOSS
+    return Outcomes.WIN
 }
 
 function getWinningType(opponent: Shape, outcome: Outcomes): Shape {
-    switch(outcome) {
-        case Outcomes.WIN:
-            switch(opponent) {
-                case Shape.ROCK: return Shape.PAPER
-                case Shape.PAPER: return Shape.SCISSORS
-                default: return Shape.ROCK
-            }
-        case Outcomes.DRAW: return opponent
-        case Outcomes.LOSS: 
-            switch(opponent) {
-                case Shape.ROCK: return Shape.SCISSORS
-                case Shape.PAPER: return Shape.ROCK
-                default: return Shape.PAPER
-            }
-    }
+    return getIndexCircular(
+        Object.values(Shape).slice(3, 6), 
+        opponent 
+            + ((outcome - 6) / 3))
+    // This is so that when the outcome is 6, it removes 0, when it's 3, it removes 1, and when it's 0, it removes 2.
 }
 
 function partOne(data: string) {
-    let matchData = parseDataPtOne(data)
-    let totalScore = 0
-    for (let match of matchData) {
-        totalScore += (decodeWin(...match) + match[1])
-    }
+    let totalScore = sum(parseDataPtOne(data).map(x => decodeWin(...x) + x[1]))
     console.log(totalScore)
     return totalScore
 }
 
 function partTwo(data: string) {
-    let matchData = parseDataPtTwo(data)
-    let totalScore = 0
-    for (let match of matchData) {
-        totalScore += match[1] + getWinningType(...match)
-    }
+    let totalScore = sum(parseDataPtTwo(data).map(x => (x[1] + getWinningType(...x))))
     console.log(totalScore)
     return totalScore
 }
+
+partOne(day2)
+
+// pretty sure one-liners are impossible
+// 1 hour later, I can safely say that it was possible.
+
+const partTwoOneLiner = (data: string) => 
+    data
+    .split("\n")
+    .map(x => x
+        .split(" ")
+        .map(y => 
+            ["A", "B", "C"].indexOf(y) === -1 ? 
+                ["X", "Y", "Z"].indexOf(y) * 3 
+                : ["A", "B", "C"].indexOf(y) + 1))
+    .map(x => x[1] 
+            + (x[0] + x[1] / 3 
+                + (x[0] + x[1] / 3 >= 2 ? -2 : 1)) 
+            % (x[0] + x[1] / 3 >= 2 ? 3 : Infinity)
+            + 1) // I don't even know wtf I just wrote, don't ask me how it works
+    .reduce((a, b) => a + b)
+
+
+    
+console.log(partTwoOneLiner(day2))

fa5a956de7cb126397c21129ecef4371c2818a79
Whitebow <whitebowfr@gmail.com>
2022-12-09 11:27:32 +0100

    Added more distance and direction utils

--- @lib/vectors/markers.ts
+++ @lib/vectors/markers.ts
@@ -1,3 +1,5 @@
+import { coords } from "."
+
 export enum DirectionMarker {
     NORTH,
     EAST,
@@ -16,4 +18,58 @@ export function updateCoords(coords: [number, number], direction: DirectionMarke
         case DirectionMarker.WEST:
             return [coords[0] - 1, coords[1]]
     }
+}
+
+export function getOppositeDirectionMarker(dir: DirectionMarker) {
+    switch(dir) {
+        case DirectionMarker.NORTH:
+            return DirectionMarker.SOUTH
+        case DirectionMarker.EAST:
+            return DirectionMarker.WEST
+        case DirectionMarker.SOUTH:
+            return DirectionMarker.NORTH
+        case DirectionMarker.WEST:
+            return DirectionMarker.EAST
+    }
+}
+
+export function getDirectionFromString(str: string) {
+    switch (str) {
+        case "U":
+            return DirectionMarker.NORTH
+        case "D":
+            return DirectionMarker.SOUTH
+        case "L":
+            return DirectionMarker.WEST
+        case "R":
+            return DirectionMarker.EAST
+    }
+}
+
+export function getMarkerDifferences(pointA: coords, pointB: coords): DirectionMarker[] {
+    let full: DirectionMarker[] = []
+    if (pointA[1] < pointB[1]) {
+        while (pointA[1] !== pointB[1]) {
+            full.push(DirectionMarker.NORTH)
+            pointA = updateCoords(pointA, DirectionMarker.NORTH)
+        }
+    } else {
+        while (pointA[1] !== pointB[1]) {
+            full.push(DirectionMarker.SOUTH)
+            pointA = updateCoords(pointA, DirectionMarker.SOUTH)
+        }
+    }
+
+    if (pointA[0] < pointB[0]) {
+        while (pointA[0] !== pointB[0]) {
+            full.push(DirectionMarker.EAST)
+            pointA = updateCoords(pointA, DirectionMarker.EAST)
+        }
+    } else {
+        while (pointA[0] !== pointB[0]) {
+            full.push(DirectionMarker.WEST)
+            pointA = updateCoords(pointA, DirectionMarker.WEST)
+        }
+    }
+    return full
 }

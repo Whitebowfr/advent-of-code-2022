7ed2b253429724d8a3a7206a21bdca6defe1f1af
Whitebow <whitebowfr@gmail.com>
2022-12-11 17:30:32 +0100

    Added day11 and 10

--- @lib/inputs/days.ts
+++ @lib/inputs/days.ts
@@ -9673,3 +9673,206 @@ D 18
 U 16
 D 9
 U 12`
+
+export const day10 = `addx 1
+addx 4
+noop
+noop
+noop
+noop
+addx 6
+addx -1
+noop
+addx 5
+noop
+addx 5
+noop
+noop
+noop
+addx 1
+addx 3
+addx 1
+addx 6
+addx -1
+noop
+noop
+noop
+addx 7
+noop
+addx -39
+noop
+noop
+noop
+addx 7
+addx 3
+addx -2
+addx 2
+noop
+addx 3
+addx 2
+addx 5
+addx 2
+addx -8
+addx 13
+noop
+addx 3
+addx -2
+addx 2
+addx 5
+addx -31
+addx 36
+addx -2
+addx -36
+noop
+noop
+noop
+addx 3
+addx 5
+addx 2
+addx -7
+addx 15
+addx -5
+addx 5
+addx 2
+addx 1
+addx 4
+noop
+addx 3
+noop
+addx 2
+addx -13
+addx -16
+addx 2
+addx 35
+addx -40
+noop
+noop
+addx 7
+noop
+noop
+noop
+addx 5
+noop
+addx 5
+addx 10
+addx -10
+noop
+noop
+noop
+addx 3
+noop
+addx 16
+addx -9
+noop
+noop
+noop
+addx 3
+noop
+addx 7
+addx -32
+addx 35
+addx -38
+addx 22
+addx 10
+addx -29
+addx 2
+noop
+addx 3
+addx 5
+addx 2
+addx 2
+addx -12
+addx 13
+noop
+noop
+addx 7
+addx 5
+noop
+noop
+noop
+addx 7
+addx -6
+addx 2
+addx 5
+addx -38
+addx 1
+noop
+noop
+addx 2
+noop
+addx 3
+addx 5
+noop
+addx 4
+addx -2
+addx 5
+addx 2
+addx 1
+noop
+addx 4
+addx 4
+addx -14
+addx 16
+noop
+addx -13
+addx 18
+addx -1
+noop
+noop
+noop`
+
+export const day11 = `Monkey 0:
+Starting items: 52, 60, 85, 69, 75, 75
+Operation: new = old * 17
+Test: divisible by 13
+  If true: throw to monkey 6
+  If false: throw to monkey 7
+
+Monkey 1:
+Starting items: 96, 82, 61, 99, 82, 84, 85
+Operation: new = old + 8
+Test: divisible by 7
+  If true: throw to monkey 0
+  If false: throw to monkey 7
+
+Monkey 2:
+Starting items: 95, 79
+Operation: new = old + 6
+Test: divisible by 19
+  If true: throw to monkey 5
+  If false: throw to monkey 3
+
+Monkey 3:
+Starting items: 88, 50, 82, 65, 77
+Operation: new = old * 19
+Test: divisible by 2
+  If true: throw to monkey 4
+  If false: throw to monkey 1
+
+Monkey 4:
+Starting items: 66, 90, 59, 90, 87, 63, 53, 88
+Operation: new = old + 7
+Test: divisible by 5
+  If true: throw to monkey 1
+  If false: throw to monkey 0
+
+Monkey 5:
+Starting items: 92, 75, 62
+Operation: new = old * old
+Test: divisible by 3
+  If true: throw to monkey 3
+  If false: throw to monkey 4
+
+Monkey 6:
+Starting items: 94, 86, 76, 67
+Operation: new = old + 1
+Test: divisible by 11
+  If true: throw to monkey 5
+  If false: throw to monkey 2
+
+Monkey 7:
+Starting items: 57
+Operation: new = old + 2
+Test: divisible by 17
+  If true: throw to monkey 6
+  If false: throw to monkey 2`
